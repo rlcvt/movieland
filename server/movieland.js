fc962d20c444c598c6507c56f537dd48cb97e536
rlcvt <rrlcohen@gmail.com>
2016-05-12 12:31:48 -0400

    Setting MAIL_URL at startup

--- server/movieland.js
+++ server/movieland.js
@@ -1,3 +1,3 @@
 Meteor.startup(function () {
-
+  process.env.MAIL_URL = "smtp://postmaster%40sandbox9e6ede951e6c445380dd21bf622735e5.mailgun.org:0aebd1dc57aef750940cb044d823b38b@smtp.mailgun.org:587";
 });
